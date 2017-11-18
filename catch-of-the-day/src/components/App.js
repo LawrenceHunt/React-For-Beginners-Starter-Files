5ccb445794acb23711a417b9e36710306666b67f
Lawrence Hunt <lawrence.hunt@hotmail.com>
2017-11-18 12:21:59 +0000

    about to add firebase config

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 
 export default class App extends React.Component {
+
   constructor() {
     super()
     this.state = {
@@ -11,6 +14,8 @@ export default class App extends React.Component {
       order: {}
     }
     this.addFish = this.addFish.bind(this)
+    this.loadSamples = this.loadSamples.bind(this)
+    this.addToOrder = this.addToOrder.bind(this)
   }
 
   addFish(fish) {
@@ -20,14 +25,42 @@ export default class App extends React.Component {
     this.setState({fishes})
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
+  addToOrder(key) {
+    const order = {...this.state.order}
+    order[key] = order[key] + 1 || 1
+    this.setState({order})
+  }
+
   render() {
     return(
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market" />
+          <Header tagline="Fresh Seafood Market"/>
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => {
+                  return (
+                    <Fish
+                      key={key}
+                      index={key}
+                      details={this.state.fishes[key]}
+                      addToOrder={this.addToOrder}
+                    />
+                  )
+                })
+            }
+          </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.addFish}/>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {formatPrice} from '../helpers'
+
+export default class Fish extends React.Component {
+
+  render() {
+    const {details, index} = this.props
+    const isAvailable = details.status === 'available'
+    const buttonText = isAvailable ? 'Add to order' : 'Sold Out!'
+
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name}/>
+        <h3 className="fish-name">{details.name}</h3>
+        <span className="price">{formatPrice(details.price)}</span>
+        <p>{details.desc}</p>
+        <button onClick={() => this.props.addToOrder(index)}>{buttonText}</button>
+      </li>
+    )
+  }
+
+}
